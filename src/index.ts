b38d69c65d46d0a8fe12979d9bc21422acd19808
Yoon Youngho <khbrst@gmail.com>
2018-09-21 01:18:15 +0900

    Adds functions.ts file

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import { testInterfaces } from "./interfaces";
 import { testClassDefaultParameter, testAbstractClass, testConstructorFunctions } from "./classes";
+import { testRestParameter, testThisParameters, testThisParametersInCallbacks, testFunctionOverloads } from "./functions";
 
 testInterfaces();
 testClassDefaultParameter();
 testAbstractClass();
 testConstructorFunctions();
+testRestParameter();
+testThisParameters();
+testThisParametersInCallbacks();
+testFunctionOverloads();
