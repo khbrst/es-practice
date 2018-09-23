5fdd9dac619ef15c7eacef7da2436a36c7f0e48f
Yoon Youngho <khbrst@gmail.com>
2018-09-23 19:46:42 +0900

    Adds enums

---
+++ src/enums.ts
@@ -0,0 +1,31 @@
+export const testReverseMappings = () => {
+  enum Enum {
+    A
+  }
+  let a = Enum.A;
+  let nameOfA = Enum[a]; // "A"
+}
+
+export const testConstEnums = () => {
+  const enum Directions {
+    Up,
+    Down,
+    Left,
+    Right
+  }
+
+  let directions = [Directions.Up, Directions.Down, Directions.Left, Directions.Right]
+}
+
+declare enum AmbientEnum {
+  A = 1,
+  B,
+  C = 2
+}
+
+export const testAmbientEnums = () => {
+  // Error!
+  // console.log(AmbientEnum.A);
+  // console.log(AmbientEnum.B);
+  // console.log(AmbientEnum.C);
+}
