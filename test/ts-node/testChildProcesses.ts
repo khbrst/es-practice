fd728be822be6eb890a8573ff02b913aee86914a
Yoon Youngho <khbrst@gmail.com>
2018-11-27 23:57:53 +0900

    Read Node.js Child Processes guide
    - Comments of `tsconfig.json` removed.

--- test/ts-node/testChildProcesses.ts
+++ test/ts-node/testChildProcesses.ts
@@ -1,5 +1,5 @@
 import { assert } from "chai";
-import { spawn } from 'child_process';
+import { fork, spawn } from 'child_process';
 
 describe('Node.js.ChildProcesses', () => {
   describe('#Introduce', () => {
@@ -15,7 +15,47 @@ describe('Node.js.ChildProcesses', () => {
       });
 
       ls.on('close', (code) => {
-        assert.deepEqual(code, 0);
+        assert.strictEqual(code, 0);
+      });
+    });
+  });
+
+  describe('#spawn', () => {
+    it('run \'ps ax | grep ssh\'', () => {
+      const ps = spawn('ps', ['ax']);
+      const grep = spawn('grep', ['ssh']);
+
+      ps.stdout.on('data', (data) => {
+        grep.stdin.write(data);
+      });
+
+      ps.stderr.on('data', (data) => {
+        assert.fail(data);
+      });
+
+      ps.on('close', (code) => {
+        assert.strictEqual(code, 0);
+        grep.stdin.end();
+      });
+
+      grep.stdout.on('data', (data) => {
+        assert.isNotNull(data.toString());
+      });
+
+      grep.stderr.on('data', (data) => {
+        assert.fail(data);
+      });
+
+      grep.on('close', (code) => {
+        assert.strictEqual(code, 0);
+      });
+    });
+
+    it('will fail', () => {
+      const subprocess = spawn('bad_command');
+
+      subprocess.on('error', (err) => {
+        assert.isNotNull(err);
       });
     });
   });
