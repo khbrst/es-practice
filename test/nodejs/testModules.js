53967b0e115474ddc4098fddcf3bd5d1287f8fa2
Yoon Youngho <khbrst@gmail.com>
2018-12-01 16:27:25 +0900

    Read Node.js Modules guide

--- test/nodejs/testModules.js
+++ test/nodejs/testModules.js
@@ -1,6 +1,11 @@
 const assert = require("chai").assert;
-const circle = require("../../src/nodejs/modules/circle");
-const Square = require("../../src/nodejs/modules/square");
+const modulesPath = "../../src/nodejs/modules";
+const a = require(modulesPath + "/a.js");
+const b = require(modulesPath + "/b.js");
+const circle = require(modulesPath + "/circle");
+const emitReady = require(modulesPath + "/emit-ready.js");
+const { hello } = require(modulesPath + "/hello.js");
+const Square = require(modulesPath + "/square");
 
 describe('Node.js.Modules', () => {
   describe('#Introduce', () => {
@@ -8,9 +13,24 @@ describe('Node.js.Modules', () => {
       assert.strictEqual(circle.area(4), 50.26548245743669);
     });
 
-    it('call member function from other module', () => {
+    it('call method from other module', () => {
       const mySquare = new Square(2);
       assert.strictEqual(mySquare.area(), 4);
+    });
+
+    it('don\'t use cyclic module', () => {
+      assert.isTrue(a.done);
+      assert.isTrue(b.done);
+    });
+
+    it('don\'t use exports in any callbacks', () => {
+      emitReady.on('ready', () => {
+        assert.fail();
+      });
+    });
+
+    it('don\'t use exports shortcut after module.exports', () => {
+      assert.isTrue(hello);
     });
   });
 });
