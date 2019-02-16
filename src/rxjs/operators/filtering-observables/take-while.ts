cc3417ffddc388f0fee9721b3195c863f2decc44
Yoon Youngho <khbrst@gmail.com>
2019-02-16 23:05:24 +0900

    Practice RxJS takeWhile operator

---
+++ src/rxjs/operators/filtering-observables/take-while.ts
@@ -0,0 +1,10 @@
+// RxJS v6+
+import { of } from 'rxjs';
+import { takeWhile } from 'rxjs/operators';
+
+// emit 1,2,3,4,5
+const source = of(1, 2, 3, 4, 5);
+// allow values until value from source is greater than 4, then complete
+const example = source.pipe(takeWhile(val => val <= 4));
+// output: 1,2,3,4
+example.subscribe(val => console.log(val));
