5de7f37cdcde1d2be54f9edc8b81022d3da0e57e
Yoon Youngho <khbrst@gmail.com>
2019-02-17 17:35:06 +0900

    Practice RxJS startWith operator

---
+++ src/rxjs/operators/combining-observables/start-with.ts
@@ -0,0 +1,10 @@
+// RxJS v6+
+import { of } from 'rxjs';
+import { startWith } from 'rxjs/operators';
+
+// emit (1,2,3)
+const source = of(1, 2, 3);
+// start with 0
+const example = source.pipe(startWith(0));
+// output: 0,1,2,3
+example.subscribe(val => console.log(val));
