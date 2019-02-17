fbcdfd5beb1bde3a71d535bd46ed413ffec61be9
Yoon Youngho <khbrst@gmail.com>
2019-02-17 17:33:42 +0900

    Practice RxJS mergeAll operator

---
+++ src/rxjs/operators/combining-observables/merge-all.ts
@@ -0,0 +1,23 @@
+// RxJS v6+
+import { of } from 'rxjs';
+import { map, mergeAll } from 'rxjs/operators';
+
+const myPromise = (val: any) =>
+  new Promise(resolve => setTimeout(() => resolve(`Result: ${val}`), 2000));
+// emit 1,2,3
+const source = of(1, 2, 3);
+
+const example = source.pipe(
+  // map each value to promise
+  map(val => myPromise(val)),
+  // emit result from source
+  mergeAll()
+);
+
+/*
+  output:
+  "Result: 1"
+  "Result: 2"
+  "Result: 3"
+*/
+example.subscribe(val => console.log(val));
