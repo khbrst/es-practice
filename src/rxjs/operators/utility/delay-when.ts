ad64f8a47445b30c37fff89a8589113b35803033
Yoon Youngho <khbrst@gmail.com>
2019-02-18 00:01:53 +0900

    Practice RxJS delayWhen operator

---
+++ src/rxjs/operators/utility/delay-when.ts
@@ -0,0 +1,13 @@
+// RxJS v6+
+import { interval, timer } from 'rxjs';
+import { delayWhen } from 'rxjs/operators';
+
+// emit value every second
+const message = interval(1000);
+// emit value after five seconds
+const delayForFiveSeconds = () => timer(5000);
+// after 5 seconds, start emitting delayed interval values
+const delayWhenExample = message.pipe(delayWhen(delayForFiveSeconds));
+// log values, delayed for 5 seconds
+// ex. output: 5s....1...2...3
+delayWhenExample.subscribe(val => console.log(val));
