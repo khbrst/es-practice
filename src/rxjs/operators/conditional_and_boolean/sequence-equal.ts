bd25bee3aab6cd8d4b6853b01ded7cc0b7ad6ff6
Yoon Youngho <khbrst@gmail.com>
2019-02-21 01:05:41 +0900

    Practice RxJS sequenceEqual operator

---
+++ src/rxjs/operators/conditional_and_boolean/sequence-equal.ts
@@ -0,0 +1,12 @@
+// RxJS v6+
+import { from, of } from 'rxjs';
+import { sequenceEqual, switchMap } from 'rxjs/operators';
+
+const expectedSequence = from([4, 5, 6]);
+
+of([1, 2, 3], [4, 5, 6], [7, 8, 9]).pipe(
+  switchMap(arr =>
+    from(arr).pipe(sequenceEqual(expectedSequence)))
+).subscribe(console.log);
+
+// output: false, true, false
