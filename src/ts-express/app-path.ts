e7b025cd71ee9c90ee471d3447f737f1247ff829
Yoon Youngho <khbrst@gmail.com>
2019-03-05 23:33:02 +0900

    Practice express app.path

---
+++ src/ts-express/app-path.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const blog = express();
+const blogAdmin = express();
+
+app.use('/blog', blog);
+blog.use('/admin', blogAdmin);
+
+console.log(app.path()); // ''
+console.log(blog.path()); // '/blog'
+console.log(blogAdmin.path()); // '/blog/admin'
